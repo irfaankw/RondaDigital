5f351c670e9fa9b50e5e29302e2edd5e3bd03802
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-05-24 15:02:20 +0800

    feat: initialize core project structure, multiapp routing, database configuration, and premium splash screen

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include('account.urls', namespace='account')),
+    # path('patrol/', include('patrol.urls', namespace='patrol')),
+    # path('emergency/', include('emergency.urls', namespace='emergency')),
+    # path('dashboard-rt/', include('dashboard_rt.urls', namespace='dashboard_rt')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

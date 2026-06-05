a381960c0c3a7cc09564d48dabe45bc9eeb3fdb3
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-06-05 15:06:31 +0800

    fix: rename alert url to darurat

--- emergency/urls.py
+++ emergency/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('cek-pending/',        views.cek_pending,        name='cek_pending'),
     path('update/<int:laporan_id>/', views.update_status, name='update_status'),
     path('riwayat/',            views.riwayat_laporan,    name='riwayat'),
-    path('alert/',              views.alert_petugas,      name='alert_petugas'),   # ← halaman
-    path('api/alert/',          views.api_alert_petugas,  name='api_alert'),       # ← AJAX
+    path('darurat/',              views.alert_petugas,      name='alert_petugas'),
+    path('api/darurat/',          views.api_alert_petugas,  name='api_alert'),
 ]

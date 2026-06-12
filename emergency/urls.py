77a421146d640f0f76d40d6f6e5fabf27919f859
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-06-12 11:18:06 +0800

    fix: clean merge conflict in verifikasi_warga.html (dashboard_rt), urls.py (emergency)

--- emergency/urls.py
+++ emergency/urls.py
@@ -7,11 +7,6 @@ urlpatterns = [
     path('cek-pending/',        views.cek_pending,        name='cek_pending'),
     path('update/<int:laporan_id>/', views.update_status, name='update_status'),
     path('riwayat/',            views.riwayat_laporan,    name='riwayat'),
-<<<<<<< HEAD
-    path('alert/',              views.alert_petugas,      name='alert_petugas'),   # ← halaman
-    path('api/alert/',          views.api_alert_petugas,  name='api_alert'),       # ← AJAX
-=======
     path('darurat/',              views.alert_petugas,      name='alert_petugas'),
     path('api/darurat/',          views.api_alert_petugas,  name='api_alert'),
->>>>>>> 524ca24b4e9a22b70aa44e7202354cbb8b013f39
 ]
